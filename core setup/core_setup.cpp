ecdaaade21f59635e7fe153844ebd7232d298302
HEMANT RAJ <149693099+HemantRaj-2005@users.noreply.github.com>
2026-07-04 11:08:37 +0530

    Refactor type definitions and main function structure
    Refactor core_setup.cpp to use type aliases and improve readability.

--- core setup/core_setup.cpp
+++ core setup/core_setup.cpp
@@ -1,9 +1,14 @@
 #include <bits/stdc++.h>
-#define int long long
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define pb push_back
 #define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+using i128 = __int128;
+using u128 = unsigned __int128;
 
 using namespace std;
 
@@ -23,13 +28,13 @@ void takeInput(vector<T> &v){
 }
 
 void solve(){
-
+// your code goes her 
 }
 
-signed main(){
+int main(){
     fast_io;
-    int t;
-    cin >> t;
+    int t = 1;
+    cin >> t; // comment this line if no multiple testcase present
     while(t--){
         solve();
     }
